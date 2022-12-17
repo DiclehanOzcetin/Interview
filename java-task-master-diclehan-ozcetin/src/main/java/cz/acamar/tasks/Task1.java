a7ed7b9f8da9d30b88f93ebf273c195b5323834a
DiclehanOzcetin <diclehan.ozcetin@gmail.com>
2022-12-17 21:30:50 +0300

    Interview questions solved
    Interview Questions submitted

---
+++ java-task-master-diclehan-ozcetin/src/main/java/cz/acamar/tasks/Task1.java
@@ -0,0 +1,28 @@
+package cz.acamar.tasks;
+
+public class Task1 {
+
+    /**
+     * Task 1. Length of the last word.
+     * <p>
+     * Given a string s consisting of some words separated by some number of spaces,
+     * return the length of the last word in the string.
+     * <p>
+     * A word is a maximal substring consisting of non-space characters only.
+     * <p>
+     * Example input: "Hello World"
+     * Expected output: 5
+     *
+     * @param str - a string s consisting of some words separated by some number of spaces.
+     * @return - the length of the last word in the string.
+     */
+    public int lengthOfLastWord(String str) {
+        str=str.trim();
+        String result="";
+        int requestedIndex=str.lastIndexOf(' ');
+        if(requestedIndex >= 0){
+            result=str.substring(requestedIndex+1);
+        }
+        return result.length();
+    }
+}
